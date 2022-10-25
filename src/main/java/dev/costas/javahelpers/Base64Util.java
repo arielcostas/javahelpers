1f4bd02102ad2ae91b4d487fec704899cd8f8f6d
Ariel Costas <a21arielcg@iesteis.es>
2022-10-25 08:49:09 +0200

    Base64Util tests

--- src/main/java/dev/costas/javahelpers/Base64Util.java
+++ src/main/java/dev/costas/javahelpers/Base64Util.java
@@ -8,8 +8,12 @@ public class Base64Util {
 	 * Encodes a string to Base64.
 	 * @param text The string to encode.
 	 * @return The encoded string.
+	 * @throws NullPointerException if {@code text} is null
 	 */
-	public static String encode(String text) {
+	public static String encode(String text) throws NullPointerException {
+		if (text == null) {
+			throw new NullPointerException("Text cannot be null");
+		}
 		return java.util.Base64.getEncoder().encodeToString(text.getBytes());
 	}
 
@@ -17,8 +21,12 @@ public class Base64Util {
 	 * Decodes a Base64 string.
 	 * @param text The string to decode.
 	 * @return The decoded string.
+	 * @throws NullPointerException if {@code text} is null
 	 */
 	public static String decode(String text) {
+		if (text == null) {
+			throw new NullPointerException("Text cannot be null");
+		}
 		return new String(java.util.Base64.getDecoder().decode(text));
 	}
 }
---
+++ src/test/java/dev/costas/javahelpers/Base64UtilTest.java
@@ -0,0 +1,50 @@
+package dev.costas.javahelpers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Base64UtilTest {
+
+	@Test
+	void test_EncodeString() {
+		var actual = Base64Util.encode("hola");
+		var expected = "aG9sYQ==";
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void test_EncodeEmpty() {
+		var actual = Base64Util.encode("");
+		var expected = "";
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void test_EncodeNull() {
+		assertThrows(NullPointerException.class, () -> Base64Util.encode(null));
+	}
+
+	@Test
+	void text_DecodeString() {
+		var actual = Base64Util.decode("aG9sYQ==");
+		var expected = "hola";
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void text_DecodeEmpty() {
+		var actual = Base64Util.decode("");
+		var expected = "";
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void test_DecodeNull() {
+		assertThrows(NullPointerException.class, () -> Base64Util.decode(null));
+	}
+}
